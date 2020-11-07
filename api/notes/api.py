f34826646e4e5d08607bf2390d958af2fbf621c6
nikto_b <niktob560@gmail.com>
2020-11-07 19:09:10 +0300

    Created note edit at make_note

--- api/notes/api.py
+++ api/notes/api.py
@@ -9,6 +9,7 @@ from security.api import AuthBearer
 
 router = Router()
 
+
 @router.post('/post_record', auth=AuthBearer())
 def post_notes_record(request, r: notes.schemas.NotesRecordIn):
     try:
@@ -50,7 +51,8 @@ def get_notes_records(request, limit: int = 100, offset: int = 0):
 @router.delete('/delete_record', auth=AuthBearer())
 def delete_notes_record(request, id: int):
     try:
-        ret = records.models.Record.objects.filter(id=id, record_type=records.models.RecordTypes.NOTE, owner_token=request.auth.owner_token).delete()
+        ret = records.models.Record.objects.filter(
+            id=id, record_type=records.models.RecordTypes.NOTE, owner_token=request.auth.owner_token).delete()
         if ret[0] == 0:
             raise ValueError('Deleted zero elements')
     except Exception as e:
@@ -59,14 +61,17 @@ def delete_notes_record(request, id: int):
     else:
         return {'result': 'success', 'code': 0}
 
+
 @router.patch('/make_note', auth=AuthBearer())
-def note_crate_record(request, id: int):
+def note_crate_record(request, id: int, new_note: records.schemas.Note = None):
     try:
         if id <= 0:
             raise ValueError('Bad id')
         ret = records.models.Record.objects.filter(
-            id=id).update(record_type=records.models.RecordTypes.NOTE, owner_token=request.auth.owner_token)
-        print(ret)
+            id=id, owner_token=request.auth.owner_token)
+        if new_note:
+            ret.update(note=new_note.note)
+        ret.update(record_type=records.models.RecordTypes.NOTE)
         if ret == 0:
             raise ValueError('Changed zero elements')
     except Exception as e:
@@ -82,7 +87,7 @@ def update_note(request, record_id: int, new_note: records.schemas.Note):
         if record_id <= 0:
             raise ValueError('Bad id')
         ret = records.models.Record.objects.filter(
-            id=record_id, record_type=records.models.RecordTypes.NOTE, owner_token=request.auth.owner_token).update(note=new_note.note)
+            id=record_id, owner_token=request.auth.owner_token, record_type=records.models.RecordTypes.NOTE).update(note=new_note.note)
         if ret == 0:
             raise ValueError('Updated zero elements')
     except ValueError as e:
